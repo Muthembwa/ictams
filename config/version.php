fb969a05e1e4c0ba46f5868b460913914bc11869
Muthembwa <mwaniki.jacob@gmail.com>
2022-02-08 09:03:49 +0300

    first commit of the seku ICT Assets Management System

---
+++ config/version.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'app_version' => 'v5.3.7',
+  'full_app_version' => 'v5.3.7 - build 6587-gcf14a0222',
+  'build_version' => '6587',
+  'prerelease_version' => '',
+  'hash_version' => 'gcf14a0222',
+  'full_hash' => 'v5.3.7-19-gcf14a0222',
+  'branch' => 'master',
+);
